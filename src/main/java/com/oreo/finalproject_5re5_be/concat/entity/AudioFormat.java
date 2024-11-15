5749dd95e1bdddcb083986dfe72cde84d06e3125
Ryu-Eun <ektl4m@gmail.com>
2024-11-15 14:20:25 +0900

    [Feat] AudioFormat 엔티티 추가

---
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/entity/AudioFormat.java
@@ -0,0 +1,38 @@
+package com.oreo.finalproject_5re5_be.concat.entity;
+
+import com.oreo.finalproject_5re5_be.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "audio_format")
+public class AudioFormat extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "audio_format_seq")
+    private Long audioFormatSeq;
+
+    @OneToOne()
+    @JoinColumn(name = "concat_result_seq")
+    private ConcatResult concatResult;
+
+    @OneToOne()
+    @JoinColumn(name = "audio_file_seq")
+    private AudioFile audioFile;
+
+    private String encoding;
+    private Integer sampleRate;
+    private Short sampleSizeBit;
+    private Short channel;
+    private Integer frameSize;
+    private Short frameRate;
+    private Character isBigEndian;
+
+}
