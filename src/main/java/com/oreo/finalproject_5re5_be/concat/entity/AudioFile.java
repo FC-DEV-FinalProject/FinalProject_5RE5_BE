c217b1072d6b3c14ad8d1708c3d862a69e07d6b5
Ryu-Eun <ektl4m@gmail.com>
2024-11-18 17:36:32 +0900

    [feat] AudioFile에 Builder 어노테이션 추가

--- src/main/java/com/oreo/finalproject_5re5_be/concat/entity/AudioFile.java
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/entity/AudioFile.java
@@ -3,15 +3,13 @@ package com.oreo.finalproject_5re5_be.concat.entity;
 
 import com.oreo.finalproject_5re5_be.global.entity.BaseEntity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @ToString
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "audio_file")
