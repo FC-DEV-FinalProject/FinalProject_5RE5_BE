951258231f5776818288c0e62759a8f2e982cc19
Ryu-Eun <ektl4m@gmail.com>
2024-11-20 11:44:23 +0900

    [feat] ConcatTabRepository에 JPQL 추가

--- src/main/java/com/oreo/finalproject_5re5_be/concat/repository/ConcatTabRepository.java
+++ src/main/java/com/oreo/finalproject_5re5_be/concat/repository/ConcatTabRepository.java
@@ -2,8 +2,14 @@ package com.oreo.finalproject_5re5_be.concat.repository;
 
 import com.oreo.finalproject_5re5_be.concat.entity.ConcatTab;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ConcatTabRepository extends JpaRepository<ConcatTab, Long> {
+    @Query("SELECT c FROM concat_tab c WHERE c.project.proSeq = :projectId")
+    Optional<ConcatTab> findByProjectId(@Param("projectId") Long projectId);
 }
