ea2e70a86fab9d0f771d14781bc64c6a5f97ec67
jongheonleee <qwefghnm1212@gmail.com>
2024-11-28 13:05:32 +0900

    [test] 테스트 클래스 추가

---
+++ src/test/java/com/oreo/finalproject_5re5_be/member/repository/MemberDeleteRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.oreo.finalproject_5re5_be.member.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class MemberDeleteRepositoryTest {
+
+
+    @Autowired
+    private MemberDeleteRepository memberDeleteRepository;
+
+    @BeforeEach
+    void setUp() {
+        // 자동주입 확인
+        assertNotNull(memberDeleteRepository);
+    }
+
+}
---
+++ src/test/java/com/oreo/finalproject_5re5_be/member/repository/MemberTermsHistoryRepositoryTest.java
@@ -0,0 +1,23 @@
+package com.oreo.finalproject_5re5_be.member.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class MemberTermsHistoryRepositoryTest {
+
+    @Autowired
+    private MemberTermsHistoryRepository memberTermsHistoryRepository;
+
+
+    @BeforeEach
+    void setUp() {
+        // 자동주입 확인
+        assertNotNull(memberTermsHistoryRepository);
+    }
+}
